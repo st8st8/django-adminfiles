98f171da890240e26e275208535198d9ce374332
Steve Bradshaw <steve@pcfive.co.uk>
2019-08-28 14:35:13 +0100

    __str__

--- adminfiles/models.py
+++ adminfiles/models.py
@@ -37,7 +37,7 @@ class FileUpload(models.Model):
         verbose_name = _('file upload')
         verbose_name_plural = _('file uploads')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     def mime_type(self):
